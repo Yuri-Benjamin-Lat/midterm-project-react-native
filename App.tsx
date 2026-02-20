8b08e6116c44ee7c53814d4977abdd0767ca5333
Yuri Benjamin Lat <yurilat3@gmail.com>
2026-02-20 20:53:03 +0800

    2nd edit forgot to add something

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import 'react-native-get-random-values'; // must be first import for uuid
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
-import { ThemeProvider, useTheme } from './context/ThemeContext';
-import { SavedJobsProvider } from './context/SavedJobsContext';
-import AppNavigator from './navigation/AppNavigation';
+import { ThemeProvider, useTheme } from './src/context/ThemeContext';
+import { SavedJobsProvider } from './src/context/SavedJobsContext';
+import AppNavigator from './src/navigation/AppNavigation';
 
 // Inner component so it can access ThemeContext
 const InnerApp: React.FC = () => {
